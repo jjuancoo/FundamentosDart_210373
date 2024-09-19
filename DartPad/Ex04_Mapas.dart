414450944467f9ae3d4323bf028ced758c1f25b1
Juanco <210373@utxicotepec.edu.mx>
2024-09-19 11:53:02 -0600

    Actualizacion del archivo Mapas

---
+++ DartPad/Ex04_Mapas.dart
@@ -0,0 +1,72 @@
+void main() {
+//Mapas
+  final pokemon = {
+    'Name': 'Pikachu',
+    'HP': 100,
+    'isAlive': true,
+    'abilities':['Impact Trueno', 'Cola de Hierro', 'Rapido'],
+    'sprites': 
+      {
+        1: "pikachu/front.png",
+        2: "pikachu/back.png"
+      }
+  };
+
+//Mapa fuertemente tipado  
+  final Map<String, dynamic>trainer = {
+    'Name': 'Ash Ketchup',
+    'Gender': 'Male',
+    'Age': 12,
+    'isGymnasiumLeader': false
+  };
+
+    final pokemons=
+  {
+    1: "Pikachu",
+    2: "Charmander", 
+    3: "Squirtle",
+    4: "Pidgeot"
+  };
+
+  
+  
+  //Imprimir los datos de un mapa
+  print(""""
+    Los datos del pokemon son usando el Mapa son:
+    --------------------------------------------
+    Pokemon = $pokemon
+    Trainer = $trainer
+    Pokemon = $pokemons
+  """);
+  print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+  print(""""
+    Nombre = ${pokemon['Name']}
+    HP = ${pokemon['HP']}
+    Vivo = ${pokemon['isAlive']}
+    Sprites = ${pokemon['sprites']}
+  """);
+  
+  
+  // Reto 01: Obtener el valor independiente de las imagenes de Front y Back del Pokemon
+  
+  // Front: ${(pokemon['sprites'])[1]} 
+  // Back: ${pokemon['sprites'][2]}
+  
+  // Dado que la solución ya esta obsoleta lo que queda es crear una nueva variable con el objeto y destructurar las imagenes para acceder a ellas
+   
+  final tmp_sprites = pokemon['sprites'] as Map<int,String>;
+  
+print("""
+  Accediendo a las propiedades del pokemo usando los braquets/corchetes [] : 
+  --------------------------------------------------
+  Nombre = ${pokemon['Name']}
+  HP = ${pokemon['HP']}
+  Vivo = ${pokemon['isAlive']}
+  Sprites = ${pokemon['sprites']}
+  ---------------------------------------------------
+  Back: ${tmp_sprites[2]}
+  Front: ${tmp_sprites[1]}
+  }
+  """);
+  
+}
